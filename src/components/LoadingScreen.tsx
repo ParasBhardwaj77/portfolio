c85e27c4d711c9819d6a6f46e8cd116dc916786c
ParasBhardwaj77 <parasbhardwaj707@gmail.com>
2026-01-12 10:23:21 +0530

    change

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+  import React, { useEffect, useState } from "react";
 
 export const LoadingScreen: React.FC<{ onComplete: () => void }> = ({
   onComplete,
